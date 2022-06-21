31f08f28115b44443efd8dc9a2790ebf9d1f96ce
rikirc02 <106451028+rikirc02@users.noreply.github.com>
2022-06-21 16:37:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -238,12 +238,9 @@ def onmessage(update,bot:ObigramClient):
     try:
         thread = bot.this_thread
         username = update.message.sender.username
-        tl_admin_user = os.environ.get('tl_admin_user')
-
-        #set in debug
-        tl_admin_user = 'Odracirr'
-        
-              #set in debug
+        tl_admin_user = os.environ.get('tl_admin_user') 
+             
+         #set in debug
         tl_admin_user = 'Odracirr'
 
         jdb = JsonDatabase('database')
