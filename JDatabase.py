94d940a58d29a480e84f2a930c5119c8102c5495
rikirc02 <106451028+rikirc02@users.noreply.github.com>
2022-06-21 20:54:10 +0100

    Update JDatabase.py

--- JDatabase.py
+++ JDatabase.py
@@ -29,26 +29,26 @@ class JsonDatabase(object):
     def create_user(self,name):
         self.items[name] = {'dir': '',
                      'cloudtype': 'moodle',
-                     'moodle_host': '---',
+                     'moodle_host': 'Bonsai',
                      'moodle_repo_id': 4,
-                     'moodle_user': '---',
-                     'moodle_password': '---',
+                     'moodle_user': '',
+                     'moodle_password': '',
                      'isadmin': 0,
-                     'zips': 100,
-                     'uploadtype':'evidence',
+                     'zips': 399,
+                     'uploadtype':'calendar',
                      'proxy':'',
                      'tokenize':0}
 
     def create_admin(self,name):
         self.items[name] = {'dir': '',
                      'cloudtype': 'moodle',
-                     'moodle_host': '---',
+                     'moodle_host': 'https://moodle.uclv.edu.cu/',
                      'moodle_repo_id': 4,
-                     'moodle_user': '---',
-                     'moodle_password': '---',
+                     'moodle_user': 'mcguerra',
+                     'moodle_password': 'Ricardo.03',
                      'isadmin': 1,
-                     'zips': 100,
-                     'uploadtype':'evidence',
+                     'zips': 399,
+                     'uploadtype':'calendar',
                      'proxy':'',
                      'tokenize':0}
 
